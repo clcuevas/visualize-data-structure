aa506355e0966af59cb772607bafae6edd3777c0
Claudia Cuevas <omordn@gmail.com>
2015-01-25 14:11:13 -0800

    working JS and CSS styles

--- insertionSort.js
+++ insertionSort.js
@@ -9,8 +9,9 @@ $(function() {
 	var addTeams = function(name, payroll) {
 		var teams = new Team(name, payroll);
 		list.push(teams);
-		$("#team-list").append("<li class='team-display'>" + teams.name + " $" + teams.payroll + "<img src='img/" + teams.name + ".png' /></li>");
-	}
+		teams.domElement = $("<li class='team-display' value='" + teams.payroll + "'>" + teams.name + " $" + teams.payroll + "<img src='img/" + teams.name + ".png' /></li>");
+		$("#unsorted").append(teams.domElement);
+	};
 
 	addTeams("Angels", 180);
 	addTeams("Dodgers", 265);
@@ -28,23 +29,58 @@ $(function() {
 	addTeams("Phillies", 130);
 	addTeams("Rangers", 132);
 
-	function insertionSort() {
-		var length, value, i, j;
+	function insertion() {
+		var len     = list.length,     // number of items in the array
+        value,                      // the value currently being compared
+        i,                          // index into unsorted section
+        j;                          // index into sorted section
+    
+    for (i=0; i < len; i++) {
+    
+        // store the current value because it may shift later
+        value = list.eq(i);
+        
+        /*
+         * Whenever the value in the sorted section is greater than the value
+         * in the unsorted section, shift all items in the sorted section over
+         * by one. This creates space in which to insert the value.
+         */
+        for (j=i-1; j > -1 && list[j] > value; j--) {
+            list[j+1] = list[j];
+        }
 
-		length = list(teams.payroll).length;
-		for (i = 0; i < length; i++) {
-			value = list[i];
-
-			for (j = i-1; (j > -1) && (list[j] > value); j--) {
-				list[j + 1] = list[j];
-			}
-			list[j + 1] = value;
-		}
-		return list;
+        list[j+1] = value;
+    }
+    
+    
 	}
+	
+	$("#sort").on("click", insertion);
 
-	$("#insertion .btn").on("click", function() {
-		insertionSort($(".team-display"));
+	$("#reset").on("click", function() {
+		history.go(0);
 	});
-
 });
+
+
+	// $("#sort").on("click", function() {
+	// 	$("li").map(function() {
+	// 		// return console.log(this.value);
+	// 		var $items = this.value;
+	// 		// return console.log($items);
+	// 		var len = list.length;
+	// 		// return console.log(len);
+	// 		var i, j, value;
+
+	// 		for (i=0; i < len; i++) {
+	// 			value = list[i];
+
+	// 			for(j = i-1; (j > -1) && ($items[j] > value); j--) {
+	// 				$items[j + 1] = $items[j];
+	// 			}
+	// 			$items[j + 1] = value;
+	// 		}
+	// 		return console.log($items);
+	// 	});
+	// });
+
---
+++ test.js
@@ -0,0 +1,89 @@
+$(function() {
+	function BaseballPayroll(team, payroll) {
+		this.team = team;
+		this.payroll = payroll;
+	}
+
+	BaseballPayroll.prototype.toHtml = function() {
+		return "<li class='team-list' data-value='" + this.payroll + "'>" + this.team + " <br>$" + this.payroll + " million<br><img src= img/" + this.team + ".png /></li>";
+	}
+
+	function Teams() {
+		thisTeams = this;
+		this.list = [
+			{"team" : "Angels", "payroll" : 145.8},
+			{"team" : "Dodgers", "payroll" : 264.9},
+			{"team" : "Yankees", "payroll" : 211},
+			{"team" : "Marlins", "payroll" : 49.7},
+			{"team" : "Red_Sox", "payroll" : 169.3},
+			{"team" : "Cubs", "payroll" : 112.1},
+			{"team" : "Cardinals", "payroll" : 111.2},
+			{"team" : "Astros", "payroll" : 63.3},
+			{"team" : "Rangers", "payroll" : 135.2},
+			{"team" : "Pirates", "payroll" : 67.7},
+			{"team" : "White_Sox", "payroll" : 111.6},
+			{"team" : "Tigers", "payroll" : 165.6},
+			{"team" : "Mets", "payroll" : 87.9},
+			{"team" : "Orioles", "payroll" : 90.9},
+			{"team" : "Rays", "payroll" : 67.9},
+			{"team" : "Giants", "payroll" : 150.8},
+			{"team" : "Nationals", "payroll" : 150.4},
+			{"team" : "Royals", "payroll" : 85.7},
+			{"team" : "Athletics", "payroll" : 68},
+			{"team" : "Mariners", "payroll" : 115.8},
+			{"team" : "Rockies", "payroll" : 85.9},
+			{"team" : "Diamondbacks", "payroll" : 71.6},
+			{"team" : "Twins", "payroll" : 100.9},
+			{"team" : "Reds", "payroll" : 99.5},
+			{"team" : "Brewers", "payroll" : 91.5},
+			{"team" : "Braves", "payroll" : 76.4},
+			{"team" : "Padres", "payroll" : 83.6},
+			{"team" : "Phillies", "payroll" : 129.8},
+			{"team" : "Blue_Jays", "payroll" : 115.9},
+			{"team" : "Indians", "payroll" : 79.2}
+		];
+
+		$.each(thisTeams.list, function() {
+			var addTeam = new BaseballPayroll(this.team, this.payroll);
+			$("#unsorted").append(addTeam.toHtml());
+		});
+	}
+
+	function insertionSort() {
+		var len, $current, i;
+		
+		len = $("li").length;
+
+		for (i = 0; i < len; i++) {
+			$current = $("li").eq(i);
+
+			while ($current.data("value") < $current.prev().data("value")) {
+				$current.prev().insertAfter($current);
+			}
+		}
+	}
+
+	var shuffle = function(m) {
+		var $temp, rand;
+		rand = Math.floor(Math.random() * m--);
+		$temp = $(".team-list:eq(" + m + ")");
+		$rand = $(".team-list:eq(" + rand + ")");
+
+		$temp.before($rand);
+		$(".team-list:eq(" + rand + ")").before($temp);
+
+		if(m) {
+			setTimeout(shuffle, 1000, m);
+		}
+	};
+
+	teams = new Teams();
+	$("#sort").on("click", function() {
+		setTimeout(insertionSort, 1000);
+	});
+
+	$("#reset").on("click", function() {
+		shuffle($("#unsorted .team-list .team-payroll").length)
+	});
+
+});
